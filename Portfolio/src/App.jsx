4023ce8abe2093c13e640501de1f9c74bc0681f0
dhurteau26 <dhurteau26@gmail.com>
2024-10-01 15:50:18 -0400

    first commit files added

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav';
+
+
+function App () {
+  return (
+    <>
+      <Nav />
+        <Outlet />
+    </>
+  );
+}
+  
+
+ 
+
+export default App;
---
+++ Portfolio/src/components/files/Home.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import 
+function Home (){
+    
+}
+
+
+
+
+
+
+
+export default Home
