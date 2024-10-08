29ab374b1697fe2bc02ebe6f9f128d445d20b5d2
dhurteau26 <dhurteau26@gmail.com>
2024-10-07 21:06:00 -0400

    new

--- Portfolio/src/components/PortfolioContainer.jsx
+++ Portfolio/src/components/PortfolioContainer.jsx
@@ -45,13 +45,13 @@ export default function PortfolioContainer() {
 
         <footer className="foot">
            <a href="https://github.com/dhurteau26">
-                <img src="./public/assets1/github.png" width="55px"></img>
+                <img src="./src/assets/github.png" width="55px"></img>
             </a>
             <a href="/">
-                <img src="./public/assets1/social.png" width="65px"></img>
+                <img src="./src/assets/social.png" width="65px"></img>
             </a>
             <a href="https://x.com/dhurteau26">
-                <img src="./public/assets1/twitter.png" width="52px"></img>
+                <img src="./src/assets/twitter.png" width="52px"></img>
             </a>
         </footer>
         </>
